e76802a580701b972b92fc19575fc8438fe512e5
SILVERSTAR <silverstar.apollo@gmail.com>
2026-04-01 11:00:57 +0800

    chore: general update
    - Deleted quartz/bindings/*, to be added again after ready
    - Erased old code for header generation inside main
    - New helper: quartz::pascal_to_camel() for converting PascalCase strings to camelCase
    - New working function: quartz::bindings::generate_header() for generating header bindings
    - Moved static initializations outside of namespace scope for generated header bindings
    - New WIP function: quartz::bindings::generate_impl()
    - Added forward declarations inside the PlayerObject header to avoid quartz::bindings::generate_header() from throwing libclang errors
    - Added headers/test/Test for testing subfolders
    - Properly catch runtime errors by simply throwing instead of 	hrow re;
    - Changed quartz::headers() to only push back regular files
    - New function quartz::try_create_file() for creating files

--- include/quartz/core/filesys.hpp
+++ include/quartz/core/filesys.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include <vector>
 #include <filesystem>
+#include <fstream>
 
 namespace quartz
 {
 
 std::vector<std::filesystem::path> headers();
-bool try_create_dir(const std::filesystem::path& path);
+void try_create_dir(const std::filesystem::path& path);
+// make sure to pass a newly initialized stream
+void try_create_file(std::ofstream& stream, const std::filesystem::path& path);
 
 } // quartz
--- src/quartz/core/filesys.cpp
+++ src/quartz/core/filesys.cpp
@@ -1,5 +1,6 @@
 #include <quartz/core/filesys.hpp>
-#include <fmt/base.h>
+#include <fmt/format.h>
+#include <stdexcept>
 
 namespace quartz
 {
@@ -7,28 +8,48 @@ namespace quartz
 std::vector<std::filesystem::path> headers()
 {
     std::vector<std::filesystem::path> ret;
-    std::filesystem::path base = "headers";
 
-    for (const auto& entry : std::filesystem::recursive_directory_iterator(base))
+    for (const auto& entry : std::filesystem::recursive_directory_iterator("headers"))
     {
-        ret.push_back(std::filesystem::relative(entry.path(), base));
+        if (!entry.is_regular_file())
+        {
+            continue;
+        }
+
+        ret.push_back(entry.path());
     }
 
     return ret;
 }
 
-bool try_create_dir(const std::filesystem::path& path)
+void try_create_dir(const std::filesystem::path& path)
 {
     std::error_code ec;
     std::filesystem::create_directories(path, ec);
 
     if (ec)
     {
-        fmt::print("Failed to create directory: \"{}\" | message: {}\n", path.string(), ec.message());
-        return false;
+        throw std::runtime_error(fmt::format("Failed to create directory: \"{}\" | error code message: {}\n", path.string(), ec.message()));
+    }
+}
+
+void try_create_file(std::ofstream& stream, const std::filesystem::path& path)
+{
+    try
+    {
+        try_create_dir(path.parent_path());
+    }
+    catch (const std::runtime_error& re)
+    {
+        throw;
     }
 
-    return true;
+    stream.open(path, std::ios::out | std::ios::trunc);
+
+    if (!stream.is_open())
+    {
+        throw std::runtime_error(fmt::format("Failed to create: \"{}\"\n", path.string()));
+    }
 }
 
 } // quartz
